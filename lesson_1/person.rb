25580ff70aec3862decfbe93dbfe4097088a5567
Brando <brandomoreno06@outlook.com>
2021-08-04 20:05:43 +0800

    modifying spaces

--- lesson_1/person.rb
+++ lesson_1/person.rb
@@ -1,13 +1,13 @@
 class Person
-    def initialize(name, age, address)
-        @name = name
-        @age = age
-        @address = address
-    end
+  def initialize(name, age, address)
+    @name = name
+    @age = age
+    @address = address
+  end
 
-    def details
-        puts "Hi #{@name}! Your age is #{@age}, and your address is #{@address}."
-    end
+  def details
+    puts "Hi #{@name}! Your age is #{@age}, and your address is #{@address}."
+  end
 end
 
 person_1 = Person.new("Juan", 21, "Ottawa, Canada")
