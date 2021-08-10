b6ad96cb3c6b34553503911ea8bcef59ace06f64
Brando <brandomoreno06@outlook.com>
2021-08-10 20:48:31 +0800

    database sql activity 2

--- lesson_3/database_1.sql
+++ lesson_3/database_1.sql
@@ -1,22 +1,54 @@
 -- CREATE TABLE
 CREATE TABLE students (
-  id integer GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) PRIMARY KEY,
+  id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   first_name character varying(255) NOT NULL,
-  middle_name character varying(255) NOT NULL,
+  middle_name character varying(255) DEFAULT 'Blank',
   last_name character varying(255) NOT NULL,
   age integer NOT NULL,
   location text NOT NULL
 );
 
 
+--INSERT RECORDS
+INSERT INTO students (first_name, last_name, age, location)
+VALUES ('Juan', 'Cruz', 18, 'Manila');
+
+INSERT INTO students (first_name, last_name, age, location)
+VALUES ('Anne', 'Wall', 20, 'Manila');
+
+INSERT INTO students (first_name, last_name, age, location)
+VALUES ('Theresa', 'Joseph', 21, 'Manila');
+
+INSERT INTO students (first_name, last_name, age, location)
+VALUES ('Isaac', 'Gray', 19, 'Laguna');
+
+INSERT INTO students (first_name, last_name, age, location)
+VALUES ('Zack', 'Matthews', 22, 'Marikina');
+
+INSERT INTO students (first_name, last_name, age, location)
+VALUES ('Finn', 'Lam', 25, 'Manila');
+
+
+
 --UPDATE TABLE
-INSERT INTO students (first_name, middle_name, last_name, age, location)
-VALUES ('Ivan', 'Ingram', 'Howard', 25, 'Bulacan');
-
-INSERT INTO students (first_name, middle_name, last_name, age, location)
-VALUES ('Avion', 'Middle', 'School', 25, 'Pampanga');
-
+UPDATE students SET first_name='Ivan', middle_name='Ingram', last_name='Howard', age=25, location='Bulacan' WHERE id=1;
 
 
 --DELETE LAST RECORD
 DELETE FROM students WHERE id=(SELECT MAX(id) FROM students);
+
+
+--DISPLAY THE COUNT OF ALL STUDENTS
+SELECT COUNT(id) FROm students;
+
+
+--DISPLAY ALL STUDENTS WITH LOCATION "MANILA"
+SELECT * FROM students WHERE location='Manila';
+
+
+--DISPLAY THE AVERAGE AGE OF ALL STUDENTS
+SELECT AVG(age) FROM students;
+
+
+--DISPLAY ALL STUDENTS ORDER BY AGE DESCENDING;
+SELECT * FROM students ORDER BY AGE DESC;
